de54c2f3b428178dc5cd70913680c8f26b7e3bf4
abimael <@AbimaelLemus>
2026-07-29 13:29:00 -0600

    0.1.2 - Abimael PL - Implementation od DI in login

--- app/src/main/java/com/example/roles/domain/usecase/InsertRegisterPersonUseCase.kt
+++ app/src/main/java/com/example/roles/domain/usecase/InsertRegisterPersonUseCase.kt
@@ -3,8 +3,9 @@ package com.example.roles.domain.usecase
 import com.example.roles.domain.model.RegisterPerson
 import com.example.roles.domain.repository.RegisterPersonRepository
 import com.example.roles.domain.repository.RemoteRecordRepository
+import javax.inject.Inject
 
-class InsertRegisterPersonUseCase(
+class InsertRegisterPersonUseCase @Inject constructor(
     private val localRepository: RegisterPersonRepository,
     private val remoteRepository: RemoteRecordRepository
 ) {
